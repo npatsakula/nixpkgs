bce5a007b169827cffd0e6f97d33449d8bac6684
Patsakula Nikita <nikita.patsakula@gmail.com>
2024-10-13 01:02:34 +0300

    libtorch-bin: 2.3.0 -> 2.4.1

--- pkgs/development/libraries/science/math/libtorch/binary-hashes.nix
+++ pkgs/development/libraries/science/math/libtorch/binary-hashes.nix
@@ -1,19 +1,19 @@
 version: builtins.getAttr version {
- "2.3.0" = {
+ "2.4.1" = {
      aarch64-darwin-cpu = {
-      name = "libtorch-macos-arm64-2.3.0.zip";
-      url = "https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.3.0.zip";
+      name = "libtorch-macos-arm64-2.4.1.zip";
+      url = "https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.4.1.zip";
       hash = "sha256-l4qY1jbsECN5qm7dWJ2jSvKuQwQ/HI6G6Vq1Kh2rxkM=";
     };
     x86_64-linux-cpu = {
-      name = "libtorch-cxx11-abi-shared-with-deps-2.3.0-cpu.zip";
-      url = "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.3.0%2Bcpu.zip";
-      hash = "sha256-dKAk6UusK2eQIcP0oMXh9cnufMpy5Ph4SGPkIPPV6ds=";
+      name = "libtorch-cxx11-abi-shared-with-deps-2.4.1-cpu.zip";
+      url = "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.4.1%2Bcpu.zip";
+      hash = "sha256-nHAJ0KhuxtsNc8oV9hl8CXeojZVPohvl3FyliJswkIA=";
     };
     x86_64-linux-cuda = {
-      name = "libtorch-cxx11-abi-shared-with-deps-2.3.0-cu121.zip";
-      url = "https://download.pytorch.org/libtorch/cu121/libtorch-cxx11-abi-shared-with-deps-2.3.0%2Bcu121.zip";
-      hash = "sha256-6B+NF6q78I2WKFudn8bK+eNYDi1zQ7mdgv06fZbm2rE=";
+      name = "libtorch-cxx11-abi-shared-with-deps-2.4.1-cu124.zip";
+      url = "https://download.pytorch.org/libtorch/cu124/libtorch-cxx11-abi-shared-with-deps-2.4.1%2Bcu124.zip";
+      hash = "sha256-/MKmr4RnF2FSGjheJc4221K38TWweWAtAbCVYzGSPZM=";
     };
   };
 }
